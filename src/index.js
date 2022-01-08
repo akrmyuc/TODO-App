86f7c5b2ad3374bb35ed471890a3c6c9b9138f41
akari <87920616+akrmyuc@users.noreply.github.com>
2022-01-08 16:54:33 +0900

    追加ボタンを実装2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // divを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグを生成
+  const li = document.createElement("li");
+  // liタグの中にテキストボックスの値を格納する
+  li.innerText = inputText;
+
+  // divタグの子要素にliを設定
+  div.appendChild(li);
+  // console.log(div);
+
+  // // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
