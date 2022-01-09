88956ab7fcbcdb2a6222a37278372415c1b4c899
akari <87920616+akrmyuc@users.noreply.github.com>
2022-01-09 21:00:41 +0900

    完了・削除ボタンを追加

--- src/index.js
+++ src/index.js
@@ -14,9 +14,19 @@ const onClickAdd = () => {
   // liタグの中にテキストボックスの値を格納する
   li.innerText = inputText;
 
-  // divタグの子要素にliを設定
+  // button(完了)タグを作成
+  const completeBotton = document.createElement("button");
+  completeBotton.innerText = "完了";
+
+  // button(削除)タグを作成
+  const deleteBotton = document.createElement("button");
+  deleteBotton.innerText = "削除";
+  console.log(deleteBotton);
+
+  // divタグの子要素に各要素を設定
   div.appendChild(li);
-  // console.log(div);
+  div.appendChild(completeBotton);
+  div.appendChild(deleteBotton);
 
   // // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
